ab464ed63c4209646b282c54bd322c64ee0456da
jhovan1 <jhovan1@users.noreply.github.com>
2017-12-22 13:01:08 -0500

    Add updated files for writing txt file

--- IISLogParser/IISLogParser/Program.cs
+++ IISLogParser/IISLogParser/Program.cs
@@ -10,11 +10,17 @@ namespace IISLogParser
     {
         static void Main(string[] args)
         {
-            string path;    // The file path for the log to read in
+            string path;            // The file path for the log to read in
+            string writeText;       // Option to write a text file
+            bool textFile;
 
             Console.WriteLine("Enter a full path and filename for the log to parse: ");
             path = Console.ReadLine();   //@"h:\u_ex171219.log"; 
             Console.WriteLine("Path entered is: " + path);
+
+            
+            
+            
 
             if (File.Exists(path))
             {
@@ -22,51 +28,79 @@ namespace IISLogParser
                 string line;
                 var newlines = new List<string>();
                 int count = 0;
+                bool exitApp = false;
 
                 Console.WriteLine("File exists.");
-                Console.WriteLine("Enter an IP address to search for: ");
-                targetIP = Console.ReadLine();
-                while ((!Regex.IsMatch(targetIP, @"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")))    
+
+                while (!exitApp)
                 {
-                    Console.WriteLine("Please enter a valid IP address");
+                    Console.WriteLine("Enter an IP address to search for, or X to exit: ");
                     targetIP = Console.ReadLine();
-                }
-
-                System.IO.StreamReader file = new System.IO.StreamReader(path);
-                while ((line = file.ReadLine()) != null)
-                {
-                    if (line.Contains(targetIP))
+                    
+                    while ((!Regex.IsMatch(targetIP, @"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")))
                     {
-                        count++;
-                        newlines.Add(line);
-                        int uriStemStart = line.IndexOf("/");
-                        int uriStemEnd = line.LastIndexOf("- 443 -");
-
-                        if (uriStemEnd < uriStemStart)
+                        if (targetIP.ToUpper() == "X")
                         {
-                            uriStemEnd = uriStemStart + 1;
+                            exitApp = true;
+                            return;
                         }
-
-                        Console.WriteLine(count + " " +
-                            "Time: " + line.Substring(0, 19) + " | " +
-                            "Method: " + line.Substring(50, 5) + " | " +
-                            "URI Stem: " + line.Substring(uriStemStart, uriStemEnd - uriStemStart)
-                        );
+                        Console.WriteLine("Please enter a valid IP address");
+                        targetIP = Console.ReadLine();
                     }
 
-                    // To write results to a file. Recommended for large result sets that exceed the screen buffer.
-                    //using (System.IO.StreamWriter dest = new System.IO.StreamWriter(@"c:\iis_log_output.txt"))
-                    //{
-                    //    foreach (var newline in newlines)
-                    //    {
-                    //        dest.WriteLine(newline);
-                    //    }
-                    //}
+                    Console.WriteLine("Do you want to save results to a text file? Enter Y or N:");
+                    writeText = Console.ReadLine();
+                    if (writeText.ToUpper().Contains("Y"))
+                    {
+                        textFile = true;
+                        Console.WriteLine("File will be written to c:\\iis_log_output.txt");
+                    }
+                    else
+                    {
+                        textFile = false;
+                    }
+
+                    System.IO.StreamReader file = new System.IO.StreamReader(path);
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        if (line.Contains(targetIP))
+                        {
+                            count++;
+                            newlines.Add(line);
+                            int uriStemStart = line.IndexOf("/");
+                            int uriStemEnd = line.LastIndexOf("- 443 -");
+
+                            if (uriStemEnd < uriStemStart)
+                            {
+                                uriStemEnd = uriStemStart + 1;
+                            }
+
+                            Console.WriteLine(count + " " +
+                                "Time: " + line.Substring(0, 19) + " | " +
+                                "Method: " + line.Substring(50, 5) + " | " +
+                                "URI Stem: " + line.Substring(uriStemStart, uriStemEnd - uriStemStart)
+                            );
+                        }
+
+                        // To write results to a file. Recommended for large result sets that exceed the screen buffer.
+                        if (textFile)
+                        {
+                            using (System.IO.StreamWriter dest = new System.IO.StreamWriter(@"c:\iis_log_output.txt"))
+                            {
+                                foreach (var newline in newlines)
+                                {
+                                    dest.WriteLine(newline);
+                                }
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("\n\rFound " + count + " instances of " + targetIP + "\n\r");
+                    count = 0;
+
                 }
-
-                Console.WriteLine("\n\rFound " + count + " instances of " + targetIP + "\n\r");
-
             }
+
             else
             {
                 Console.WriteLine("File does not exist.");
