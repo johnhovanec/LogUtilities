202988d7819454fff3f55c2d61ca2af43f666ae8
jhovan1 <jhovan1@users.noreply.github.com>
2017-12-22 12:38:01 -0500

    Add IIS log parser files; update README

---
+++ IISLogParser/IISLogParser/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace IISLogParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;    // The file path for the log to read in
+
+            Console.WriteLine("Enter a full path and filename for the log to parse: ");
+            path = Console.ReadLine();   //@"h:\u_ex171219.log"; 
+            Console.WriteLine("Path entered is: " + path);
+
+            if (File.Exists(path))
+            {
+                string targetIP;
+                string line;
+                var newlines = new List<string>();
+                int count = 0;
+
+                Console.WriteLine("File exists.");
+                Console.WriteLine("Enter an IP address to search for: ");
+                targetIP = Console.ReadLine();
+                while ((!Regex.IsMatch(targetIP, @"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")))    
+                {
+                    Console.WriteLine("Please enter a valid IP address");
+                    targetIP = Console.ReadLine();
+                }
+
+                System.IO.StreamReader file = new System.IO.StreamReader(path);
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains(targetIP))
+                    {
+                        count++;
+                        newlines.Add(line);
+                        int uriStemStart = line.IndexOf("/");
+                        int uriStemEnd = line.LastIndexOf("- 443 -");
+
+                        if (uriStemEnd < uriStemStart)
+                        {
+                            uriStemEnd = uriStemStart + 1;
+                        }
+
+                        Console.WriteLine(count + " " +
+                            "Time: " + line.Substring(0, 19) + " | " +
+                            "Method: " + line.Substring(50, 5) + " | " +
+                            "URI Stem: " + line.Substring(uriStemStart, uriStemEnd - uriStemStart)
+                        );
+                    }
+
+                    // To write results to a file. Recommended for large result sets that exceed the screen buffer.
+                    //using (System.IO.StreamWriter dest = new System.IO.StreamWriter(@"c:\iis_log_output.txt"))
+                    //{
+                    //    foreach (var newline in newlines)
+                    //    {
+                    //        dest.WriteLine(newline);
+                    //    }
+                    //}
+                }
+
+                Console.WriteLine("\n\rFound " + count + " instances of " + targetIP + "\n\r");
+
+            }
+            else
+            {
+                Console.WriteLine("File does not exist.");
+                return;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,9 +5,11 @@
 ### Amazon Log parser
 #### Two versions, a Bash and a Python version. The Bash version was too slow for large logs, so I tried Python and it ran blazingly fast.
 
+### IIS Log parser
+#### C# console application to parse an IIS log file based on IP address. Returns the date, method and uri stem.
 
 ### Mac Logon Script
 #### If you ever need to have Macs in an Active Directory domain mount network shares, this will do it. Also includes a plist file to dictate when the script is run.
 
 ### Bash SMTP Telnet tests
-#### A basic Telnet script to test SMTP, POP#, and IMAP for a given IP address with some fancy color coding.
+#### A basic Telnet script to test SMTP, POP3, and IMAP for a given IP address with some fancy color coding.
